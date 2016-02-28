57a0c6e15a55917e14e8536e1916b951fd08c386
Stephen Chin <ikmujn780@gmail.com>
2016-02-28 01:18:00 -0500

    Just Started
    Turned this into a git repository

---
+++ A test in C++.cpp
@@ -0,0 +1,10 @@
+// http://www.cprogramming.com/tutorial/java/syntax-differences-java-c++.html
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//printf("Hello World!");
+	cout << "Hello World!" << endl;
+}
---
+++ Text based adventure.py
@@ -0,0 +1,26 @@
+"""This will be my sandbox for creating a text adventure game.
+Not exactly sure how to do it, but we will start with something anyway."""
+
+class Room():
+	def __init__(self, name):
+		self.name = name
+
+roomlist = {
+	1 : Room("LivingRoom"),
+	2 : Room("Kitchen"),
+	3 : Room("Bedroom"),
+	4 : Room("Bathroom")
+}
+
+def starting_statements():
+	print ("This is a test of something new.")
+	print ("Please enjoy.\n")
+
+	print ("You are sitting on the couch in the LivingRoom and you decide that you are getting hungry.")
+
+def main():
+	starting_statements()
+	playing = True
+	answer = input("What would you like to do?")
+
+main()
